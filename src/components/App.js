3dfd0bcdb79d57bacf6f13c12f1f8528cb642457
Efy Neftali Hernandez <efy.hernandez@gmail.com>
2022-07-12 23:19:45 -0500

    base app.js  rendering working

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+// import GalleryItems from './GalleryItem'
+// import SearchForm from './SearchForm'
+
+
+function App() {
+  return (
+     <h1>hello guys</h1>
+    // <GalleryItems>
+    //   
+    // </GalleryItems>
+    // <GalleryItems/>
+    // <Nav/>
+    // <SearchForm/>
+    
+     
+    ) 
+  
+}
+
+export default App
---
+++ src/components/Gallary.js
@@ -0,0 +1,9 @@
+// //classes as modules
+// import React, {Component} from 'react'
+
+// //declare the claas as a module
+// class Gallary extends Component{
+
+// }
+
+// export default Gallary
